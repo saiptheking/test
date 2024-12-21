26788754f8741b5da1d9c6799a146445320bd600
saip_the_king <86388371+saiptheking@users.noreply.github.com>
2024-12-21 03:04:05 +0000

    test

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
 sudo apt-get update  -y
 sudo apt-get upgrade  -y
-sudo apt-get install audacity 
-sudo apt-get install lmms
+sudo apt-get install audacity -y
+sudo apt-get install lmms -y
+sudo apt-get install calibre -y
+sudo apt-get install firefox-esr -y
+sudo apt-get install kdenlive -y
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install musescore -y
