95b80bb8f843f9479176e018d3ca4f0927ee4bcf
saip_the_king <86388371+saiptheking@users.noreply.github.com>
2024-12-21 19:35:43 +0000

    t

--- script.sh
+++ script.sh
@@ -7,3 +7,24 @@ sudo apt-get install firefox-esr -y
 sudo apt-get install kdenlive -y
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install musescore -y
+sudo apt-get install libreofffice -y
+sudo apt-get install inkscape -y
+sudo apt-get install gimp -y
+sudo apt-get install uget -y
+sudoa apt-get install freecad -y
+sudo apt-get install idle -y
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install flatpak -y
+sudo apt-get update && sudo apt-get upgrade -y
+flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub eu.nokun.MirrorHall -y
+flatpak install flathub md.obsidian.Obsidian -y
+flatpak install flathub org.gnome.OCRFeeder -y
+flatpak install flathub com.tominlab.wonderpen -y
+flatpak install flathub net.codeindustry.MasterPDFEditor -y
+flatpak install flathub org.openshot.OpenShot -y
+flatpak install flathub com.wonderlandengine.editor -y
+flatpak install flathub io.typora.Typora -y
+sudo apt-get update && sudo apt-get upgrade -y
+git clone https://github.com/joedefen/crostini-kde-setup.git
+bash crostini-kde-setup/kde-setup.sh
